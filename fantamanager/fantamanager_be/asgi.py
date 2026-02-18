c780a01daaff1d6f31b1fc0ed4c0cf6c4286aa69
enricobasciu <enrico.basciu@abinsula.com>
2026-02-18 11:37:02 +0100

    refactor

--- fantamanager/fantamanager_be/asgi.py
+++ fantamanager/fantamanager_be/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fantamanager project.
+ASGI config for fantamanager_be project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantamanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantamanager_be.settings')
 
 application = get_asgi_application()
--- fantamanager/fantamanager_be/urls.py
+++ fantamanager/fantamanager_be/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for fantamanager project.
+URL configuration for fantamanager_be project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
